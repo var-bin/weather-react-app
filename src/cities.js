7294f355981c332810c46693c8d8905bc30c0a85
Innely <innalazar@gmail.com>
2021-08-20 17:47:37 +0300

    cityComponent 20.08.2021

---
+++ src/cities.js
@@ -0,0 +1,38 @@
+export const cities =[
+    {
+        id: 1,
+        img: 'https://lh3.googleusercontent.com/proxy/6yDUtM9KdDGnpuWBdHWxYtkZjg71f4v1qqeSRbLbGQ9qQx3b8NicFIpHJsNn7IjDSectxRryz5cVL2lT9ogf0iZe72i006n01Ul3nGFBan5IRA0XqFVd3AA',
+        city: 'Berlin',
+        country: 'Germany',
+    },
+    {
+        id: 2,
+        img: 'https://tournavigator.pro/%D1%84%D0%BE%D1%82%D0%BE/other/1001/1002/1545078882.jpg',
+        city: 'Paris',
+        country: 'France',
+    },
+    {
+        id: 3,
+        img: 'https://immigrant.today/images/2017-01-28-134751-9db881bf9.jpg',
+        city: 'New York',
+        country: 'United States',
+    },
+    {
+        id: 4,
+        img: 'https://lenta.ua/storage/10/08/11/30/890_450_5e2f2201e72ea.jpg',
+        city: 'London',
+        country: 'England',
+    },
+    {
+        id: 5,
+        img: 'https://www.orangesmile.com/common/img_cities_w680/sao-paulo--671824-4.jpg',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+    },
+    {
+        id: 6,
+        img: 'https://wikiway.com/upload/iblock/2e6/Tokio-s-vysoty.jpg',
+        city: 'Tokyo',
+        country: 'Japan',
+    },
+];
