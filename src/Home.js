1af46b045dc34c2b7f2d3116f4813e2df2633fdf
Innely <innalazar@gmail.com>
2021-09-09 09:55:24 +0300

    api fetch 09.09.2021

--- src/Home.js
+++ src/Home.js
@@ -1,71 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, /* useReducer */ } from 'react';
 import { useGetAxios } from './useGetAxios';
 import { createApi } from 'unsplash-js';
+//import { reducer } from './reducer';
 import City from './City';
 
 const url = 'http://dataservice.accuweather.com/currentconditions/v1/topcities/50?apikey=r0f1L4gMsX9SDBDfkv3b8tlkPCBILlvL&language=en';
-/* const cities = ["Dhaka", "Kinshasa", "Santiago", "Beijing", "Bogota", "Berlin",
-"Cairo", "Madrid", "London", "Athens", "Hong Kong", "Jakarta", "Baghdad", "Tokyo",
-"Seoul", "Mexico City", "Lima", "Moscow", "Riyadh", "Singapore", "Bangkok", "Hanoi",
-"Sydney", "Shanghai", "Delhi", "Bengaluru", "Mumbai", "Kolkata", "Karachi", "Istanbul",
-"Los Angeles", "New York", "Kabul", "Buenos Aires", "Havana", "Paris", "Tehran", "Manila",
-"Belgrade", "Dakar", "Taipei City", "Cape Town", "São Paulo", "Toronto",
-"Osaka-shi", "Jerusalem", "Amsterdam", "Stockholm", "Honolulu", "Reykjavik"]; */
 const unsplash = createApi({ accessKey: 'UFb-0W1ebRAVU6jawg9txBoQf633c4t8tA7TRvpDb88' });
 
 export const getUnsplashPhotos = () => {
-    const [photos, setPhotos] = useState([]);
+    const [dataPhotos, setDataPhotos] = useState([]);
+    const [loading, setLoading] = useState(true);
     const data = useGetAxios(url);
-    let data1 = [];
+    let dataWeather = [];
 
+    // check for uploaded datas from weather API
     if (data?.cities && !data.loading) {
-        data1 = data.cities.slice(0, 2);
+        dataWeather = data.cities.slice(0, 1);
     }
 
-    /* const allPhotos = data.cities.map((city, cityIndex) => {
-        if (cityIndex < 5) {
+    const photoPromises = dataWeather.map((city, index) => {
+        if (index < 1) {
             return unsplash.photos.getRandom({
                 query: city.EnglishName,
-                count: 1
-            }).then(result => {
-                if (result?.response) {
-                    setPhotos(result.response);
-                }
-            }).catch(error => console.log('error occurred: ', error));
+                count: 1,
+                orientation: 'landscape',
+            });
         }
-    }); */
+    }); 
 
-    // Promise.all(allPhotos);
-
-    // getting a new list of cities depending on the url change
     useEffect(() => {
         (async () => {
-            // setPhotos(Promise.all(allPhotos)) instead of next
-            unsplash.photos.getRandom({
-                query: 'Paris',
-                count: 1
-            }).then(result => {
-                if (result?.response) {
-                    setPhotos(result.response);
+            await Promise.all(photoPromises).then(values => {
+                if (values?.length) {
+                    // values = [valueOfPromise1, valueOfPromise2, ...]
+                    console.log('values', values);
+                    setDataPhotos(values.map((item) => item.response[0]));
+                    setLoading(false); 
                 }
-            }).catch(error => console.log('error occurred: ', error));
+            }).catch(error => {
+                // rejectReason of any first rejected promise
+                console.log('error occurred: ', error); 
+            });  
         })();
-
     }, []);
+    
 
-    return { photos, data: data1 };
+    return { dataPhotos, data: dataWeather, loading: loading};
 };
 
 
 const Home = () => {
-    const { photos, data } = getUnsplashPhotos('Paris');
+    const { dataPhotos, data, loading } = getUnsplashPhotos();
+    console.log('dataPhotos', dataPhotos);
+    console.log('data', data);
+    console.log('loading', loading);
+    console.log('--------------------------------');
 
-    if (photos?.length ) {
-        console.log('data: ', data);
-        console.log('photos:', photos);
-    }
+   /*  const defaultState = {
+        cities: myData,
+        favorites: [],
+    };
+    const [state, dispatch] = useReducer(reducer, defaultState);
+ */
+   /*  useEffect(() => {
+        // set {favorites} to local storage 
+        localStorage.setItem('favorites', JSON.stringify(state.favorites));
+    }, [state.favorites]); */
 
-    if (data.loading) return (
+
+    if (loading || !dataPhotos.length) return (
         <div className="container pt-4 pb-4">
             <div className="row row-cols-1 row-cols-lg-2 g-4">
             loading...
@@ -76,15 +79,13 @@ const Home = () => {
     return (
         <div className="container pt-4 pb-4">
             <div className="row row-cols-1 row-cols-lg-2 g-4">
-                {data.map((city) => {
+                {data.map((city, index) => {
                     return (
                         <City
                             {...city}
                             key={city.Key}
 
-                            //img={urls.regular}
-                            img="https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=1080&fit=max"
-
+                            img={dataPhotos[index].urls.small}
                             city={city.LocalizedName}
                             country={city.Country.LocalizedName}
                             isFavorite={false}
@@ -92,8 +93,16 @@ const Home = () => {
                             weatherIcon={city.WeatherIcon}
                             weatherTemperatureUnit={city.Temperature.Metric.Unit}
                             weatherTemperatureValue={city.Temperature.Metric.Value}
-                        >
-                        </City>
+                         /*     onIsFavorite={() =>  
+                                dispatch({ 
+                                    type: 'ADD_FAVORITE_ITEM', 
+                                    id: city.id,
+                                    img: city.img,
+                                    city: city.city,
+                                    country: city.country,
+                                })
+                            } */  
+                        ></City>
                     );
                 })}
             </div>
